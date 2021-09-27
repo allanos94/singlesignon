13335158bbdeb62a1517c90293a8dcc10692cefa
Eshwari Chandrashekhar <eshwari@wazoku.com>
2021-09-27 14:46:27 +0100

    Added missing files and routes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from polls.views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('polls/', include('polls.urls')),
